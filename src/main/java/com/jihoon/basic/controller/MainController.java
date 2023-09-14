cb24f26f1b5b9792602dfd166834b767964c9c63
devjiraynor <jiraynor@naver.com>
2023-09-14 11:51:09 +0900

    2023.09.14. 11:50

--- src/main/java/com/jihoon/basic/controller/MainController.java
+++ src/main/java/com/jihoon/basic/controller/MainController.java
@@ -1,5 +1,11 @@
 package com.jihoon.basic.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +25,53 @@ public class MainController {
     @RequestMapping(value="hello", method={RequestMethod.POST})
     public String hello() {
         return "Hello Spring framework!!";
+    }
+
+    // description: @RequestMapping 중 GET Method에 한정하여 인식 //
+    // description: 데이터를 얻기 위한 요청 //
+    // description: 데이터 입력시 URL로 입력 //
+    @GetMapping("")
+    public String getMethod() {
+        return "This method is Get Method";
+    }
+
+    // description: @RequestMapping 중 Post Method에 한정하여 인식 //
+    // description: 데이터를 생성하기 위한 요청 //
+    // description: 데이터 입력시 Body로 입력 //
+    @PostMapping("")
+    public String postMethod() {
+        return "This method is Post Method";
+    }
+
+    // description: @RequestMapping 중 PUT Method에 한정하여 인식 //
+    // description: 데이터를 수정하기 위한 요청 (전체를 수정) //
+    // description: 데이터 입력시 Body로 입력 //
+    @PutMapping("")
+    public String putMethod() {
+        return "This mehtod is Put method";
+    }
+
+    // description: @RequestMapping 중 PATCH Method에 한정하여 인식 //
+    // description: 데이터를 수정하기 위한 작업 (일부만 수정) //
+    // description: 데이터 입력시 Body로 입력 //
+    @PatchMapping("")
+    public String patchMethod() {
+        return "This method is Patch method";
+    }
+
+    // description: @RequestMapping 중 Delete Method에 한정하여 인식 //
+    // description: 데이터를 삭제하기 위한 작업 //
+    // description: 데이터 입력시 URL로 입력 //
+    @DeleteMapping("")
+    public String deleteMethod() {
+        return "This method is Delete method";
+    }
+
+    @GetMapping("path-variable/{variable}")
+    public String getPathVariable(
+        @PathVariable("variable") String variable
+    ) {
+        return "Parameter value : " + variable;
     }
     
 }
