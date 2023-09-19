a7bc8a365820b16a0b8430a7884cd549d51e78ab
devjiraynor <jiraynor@naver.com>
2023-09-19 13:07:40 +0900

    2023.09.19. 12:50

--- src/main/java/com/jihoon/basic/repository/UserRepository.java
+++ src/main/java/com/jihoon/basic/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.jihoon.basic.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jihoon.basic.entity.UserEntity;
@@ -21,5 +22,38 @@ public interface UserRepository extends JpaRepository<UserEntity, String> {
     UserEntity findByEmailAndNickname(String email, String nickname);
     // SELECT * FROM user WHERE address_detail = '??' ORDER BY address DESC;
     List<UserEntity> findByAddressDetailOrderByAddressDesc(String addressDetail);
+
+    boolean existsByEmail(String email);
+    // SELECT * FROM user WHERE email = '?' OR nickname = '?' OR tel_number = '?' -> 레코드가 존재하는지
+    boolean existsByEmailOrNicknameOrTelNumber(String email, String nickname, String telNumber);
+
+    long countByAddress(String address);
+
+    // description: @Query - 쿼리 메서드만으로 데이터베이스 작업을 수행할 수 없을 때 사용하는 어노테이션 //
+    // description: JPQL (Java Persistence Query Languege) - SQL과 문법은 유사하지만 데이터베이스 테이블이 아니는 엔터티 클래스 기준으로 쿼리를 작성 //
+    // description: Native Query - SQL //
+    
+    // JPQL
+    // SELECT * FROM user WHERE email = '??';
+    @Query(value="SELECT u FROM user u WHERE u.email = ?1")
+    UserEntity findByEmailJPQL(String email);
+    // SQL
+    @Query(value="SELECT * FROM user WHERE email = ?1", nativeQuery=true)
+    UserEntity findByEmailSQL(String email);
+
+    // SELECT * FROM user WHERE email = '??' AND nickname = '??';
+    // JPQL
+    @Query(value="SELECT u FROM user u WHERE u.email = ?1 AND u.nickname = ?2")
+    UserEntity findByEmailAndNicknameJPQL(String email, String nickname);
+
+    @Query(value=
+        "SELECT * " + 
+        "FROM user " +
+        "WHERE email IN ( " +
+        "   SELECT DISTINCT writer_email " +
+        "   FROM board " +
+        ")"    
+    , nativeQuery=true)
+    List<UserEntity> getBoardWriterUserList();
 
 }
